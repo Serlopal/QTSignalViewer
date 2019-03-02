f02e74e048a5d419b6e7c022c3ba9bc81ff7270c
Serlopal <oro_sergio@hotmail.com>
2019-03-02 01:06:23 +0000

    initial implementation of the signal viewer.

---
+++ main.py
@@ -0,0 +1,74 @@
+
+
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import pyqtSignal
+
+import pyqtgraph as pg
+import numpy as np
+from threading import Thread
+import time
+import collections
+
+class SignalViewer(QApplication):
+	emitter_signal = pyqtSignal(object)
+
+	def __init__(self, feeder, num_signals):
+		super().__init__([])
+		# save feeder function to generate or fetch data to be plot
+		self.feeder = feeder
+		# save number of signals
+		self.nplots = num_signals
+		# set number of samples to be displayed per signal at a time
+		self.nsamples = 500
+		# connect the signal to be emitted by the feeder to the slot of the plotWidget that will update the signals
+		self.emitter_signal.connect(lambda values: self.update(values))
+		# buffer to store the data from all signals
+		self.buff = np.zeros((self.nplots, self.nsamples))
+		# create a main window of the UI
+		self.window = QMainWindow()
+		# create a plot widget inside the main window
+		self.p = pg.PlotWidget()
+		self.window.setCentralWidget(self.p)
+		# create curves for the signals
+		self.curves = []
+		for i in range(self.nplots):
+			c = pg.PlotCurveItem(pen=(i, self.nplots * 1.3))
+			self.p.addItem(c)
+			self.curves.append(c)
+		# launch feeder thread
+		self.start_feeder()
+
+	def update(self, data):
+		# update buffer
+		self.buff = np.concatenate([self.buff[:, 1:], np.reshape(data, (-1, 1))], axis=1)
+		# update plots
+		for i in range(self.nplots):
+			self.curves[i].setData(self.buff[i])
+
+	def feeder_emitter(self):
+		while True:
+			time.sleep(0.033)
+			data = self.feeder()
+			self.emitter_signal.emit(data)
+
+	def start_feeder(self):
+		feeder_thread = Thread(target=self.feeder_emitter)
+		feeder_thread.start()
+
+	def start(self):
+		self.window.show()
+		self.exec_()
+
+	def update_signals(self, values):
+		self.emitter_signal.emit(values)
+
+def feed_data():
+	return np.random.normal(size=10)
+
+if __name__ == "__main__":
+
+	feeder = feed_data
+	viewer = SignalViewer(feeder=feed_data, num_signals=10)
+	viewer.start()
+
